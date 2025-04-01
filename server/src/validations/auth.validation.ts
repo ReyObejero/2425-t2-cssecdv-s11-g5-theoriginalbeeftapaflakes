d29969f5217119157c456c1d536271c176ca1eb1
Rey Obejero <rey_obejero@dlsu.edu.ph>
2025-04-02 03:31:19 +0800

    feat(api): Enforce password complexity and length requirements based on the DLSU Active Directory (AD) standards

--- server/src/validations/auth.validation.ts
+++ server/src/validations/auth.validation.ts
@@ -8,6 +8,20 @@ export const loginSchema = z.object({
     }),
 });
 
+/**
+ * @remarks
+ *
+ * This schema enforces the password complexity and length standards outlined in the DLSU
+ * Active Directory (AD) with some modifications. The specific requirements are listed below:
+ * - Minimum length should be at least 8
+ * - Number of special characters to include 1
+ * - Must contain at least 1 upper case character(s)
+ * - Number of numerals to include 1
+ * - Must contain at least 1 lower case character(s)
+ *
+ *
+ * Password regex is adopted from: {@link https://stackoverflow.com/a/21456918}
+ */
 export const registerSchema = z.object({
     body: z.object({
         username: z
@@ -16,6 +30,9 @@ export const registerSchema = z.object({
         email: z.string({ message: errorMessages.INVALID_INPUT }).email({ message: errorMessages.INVALID_INPUT }),
         password: z
             .string({ message: errorMessages.INVALID_INPUT })
-            .min(MIN_PASSWORD_LENGTH, { message: errorMessages.INVALID_INPUT }),
+            .min(MIN_PASSWORD_LENGTH, { message: errorMessages.INVALID_INPUT })
+            .regex(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'), {
+                message: errorMessages.INVALID_INPUT,
+            }),
     }),
 });
